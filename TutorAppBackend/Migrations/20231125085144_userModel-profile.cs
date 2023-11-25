a90c18ca55bb335b4bc97d01148891f70ebfe8b3
terrynuechterlein <terrynuechterlein@gmail.com>
2023-11-25 03:52:26 -0500

    migration

---
+++ TutorAppBackend/Migrations/20231125085144_userModel-profile.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutorAppBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class userModelprofile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Grade",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "School",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Website",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Grade",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "School",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Website",
+                table: "AspNetUsers");
+        }
+    }
+}
